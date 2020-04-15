4832badabb3dc691f73fae303cfaa40a06bfd7a3
Venkat Shiva <venkata.shiva@active.ai>
2020-04-15 16:28:35 +0530

    added eslint airbnb to project

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const chalk = require('chalk');
 const path = require('path');
 
 const app = express();
-app.use(express.static(path.join(__dirname,'public')));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname,'views','index.html'));
-})
-const port = process.env.PORT || 8000; 
-app.listen( port, function(){
-    console.log(`Server started and listening on  ${chalk.green(port)}`);
-})
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server started and listening on  ${chalk.green(port)}`);
+});
