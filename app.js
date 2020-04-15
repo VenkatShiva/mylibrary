cf0865a27c67d0edab0e3c28e3ab6c216c761ef8
Venkat Shiva <venkata.shiva@active.ai>
2020-04-15 15:59:24 +0530

    added static files

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const chalk = require('chalk');
+const path = require('path');
 
 const app = express();
+app.use(express.static(path.join(__dirname,'public')));
 app.get('/',function(req,res){
-    res.send('Hello from My Library App..!');
+    res.sendFile(path.join(__dirname,'views','index.html'));
 })
 const port = process.env.PORT || 8000; 
 app.listen( port, function(){
-    console.log(`Server started and listening on ${port}`);
+    console.log(`Server started and listening on  ${chalk.green(port)}`);
 })
